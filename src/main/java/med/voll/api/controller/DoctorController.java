3e6b96db0bb871a7165d886fa30517fa08f6b10d
Bryan Santos <sp.bryansantos@gmail.com>
2024-03-13 20:08:32 -0300

    Doctor listing

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -3,14 +3,17 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.address.Address;
 import med.voll.api.doctor.Doctor;
+import med.voll.api.doctor.DoctorListingData;
 import med.voll.api.doctor.DoctorRegistrationData;
 import med.voll.api.doctor.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("doctors")
@@ -23,5 +26,10 @@ public class DoctorController {
   @Transactional
   public void register(@RequestBody @Valid DoctorRegistrationData data) {
     repository.save(new Doctor(data));
+  }
+
+  @GetMapping
+    public Page<DoctorListingData> list(@PageableDefault(size = 10, sort = {"name"}) Pageable pagination) {
+    return repository.findAll(pagination).map(DoctorListingData::new);
   }
 }
