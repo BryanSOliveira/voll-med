6ec79e7a49dd588e8d310fa503d6fc3de3b27251
Bryan Santos <sp.bryansantos@gmail.com>
2024-05-04 11:17:55 -0300

    Added API documentation using SpringDoc

--- src/main/java/med/voll/api/controller/AppointmentController.java
+++ src/main/java/med/voll/api/controller/AppointmentController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import med.voll.api.domain.appointment.AppointmentDetailsData;
 import med.voll.api.domain.appointment.AppointmentSchedule;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("appointments")
+@SecurityRequirement(name = "bearer-key")
 public class AppointmentController {
 
   @Autowired
